a2a15fe879bf74c4188e3ba4880062a8ab1cfcd1
Bryce72 <brycedunlapsf56@gmail.com>
2024-08-07 15:39:44 -0400

    added main

--- Main.java
+++ Main.java
@@ -1,3 +1,20 @@
+import java.util.List;
+
 public class Main {
 
+    public static void main(String[] args) {
+
+        //instantiate rbt
+
+        RedBlackTree rbTree = new RedBlackTree();
+
+        //recipe
+        Recipe beefWellington = new Recipe("Beef Wellington", List.of("Beef", "Flour", "Eggs"));
+
+
+        rbTree.put(beefWellington.getName().hashCode(), beefWellington);
+
+
+
+    }
 }
